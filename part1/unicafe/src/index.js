4aea354ef407064dab3cd3c65440c8423931df4f
qux-bbb <1147635419@qq.com>
2020-07-07 00:33:20 +0800

    1.6

---
+++ part1/unicafe/src/index.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react'
+import ReactDOM from 'react-dom'
+
+const Head = (props) => <h1>{props.text}</h1>
+
+const Button = (props) => (
+  <button onClick={props.handleClick}>
+    {props.text}
+  </button>
+)
+
+const App = () => {
+  // save clicks of each button to own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+
+
+  return (
+    <div>
+      <Head text="give feedback" />
+      <Button handleClick={() => setGood(good+1)} text="good" />
+      <Button handleClick={() => setNeutral(neutral+1)} text="neutral" />
+      <Button handleClick={() => setBad(bad+1)} text="bad" />
+      <Head text="statistics" />
+      <p>good {good}</p>
+      <p>neutral {neutral}</p>
+      <p>bad {bad}</p>
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, 
+  document.getElementById('root')
+)
