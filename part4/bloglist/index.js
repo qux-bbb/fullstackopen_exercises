571f3b688994025878d30a63e5dad235bdaa09c2
qux-bbb <1147635419@qq.com>
2020-10-27 15:10:45 +0800

    extract blog model

--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -5,15 +5,7 @@ const cors = require('cors')
 const mongoose = require('mongoose')
 const config = require('./utils/config')
 const logger = require("./utils/logger")
-
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
-
-const Blog = mongoose.model('Blog', blogSchema)
+const Blog = require("./models/blog")
 
 const mongoUrl = config.MONGODB_URI
 mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
