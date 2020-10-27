21bf87fa6a9da572e7f77d99f5adcd71302d49a2
qux-bbb <1147635419@qq.com>
2020-10-27 15:55:38 +0800

    extract app.js, 4.2 finished

---
+++ part4/bloglist/app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const config = require('./utils/config')
+const blogsRouter = require('./controllers/blogs')
+
+const mongoUrl = config.MONGODB_URI
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+
+app.use(cors())
+app.use(express.json())
+app.use('/api/blogs', blogsRouter)
+
+module.exports = app
--- part4/bloglist/index.js
+++ part4/bloglist/index.js
@@ -1,18 +1,10 @@
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
+const http = require('http')
 const config = require('./utils/config')
 const logger = require('./utils/logger')
-const blogsRouter = require('./controllers/blogs')
+const app = require('./app')
 
-const mongoUrl = config.MONGODB_URI
-mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+const server = http.createServer(app)
 
-app.use(cors())
-app.use(express.json())
-app.use('/api/blogs', blogsRouter)
-
-app.listen(config.PORT, () => {
+server.listen(config.PORT, () => {
   logger.info(`Server running on port ${config.PORT}`)
 })
