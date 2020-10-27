7f810ebff4eb9862e71c760aa3a2457a4b364dce
qux-bbb <1147635419@qq.com>
2020-10-27 19:13:07 +0800

    4.5

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -10,8 +10,29 @@ const totalLikes = (blogs) => {
 
   return blogs.reduce(reducer, 0)
 }
+
+const favoriteBlog = (blogs) => {
+  let maxLike = -1
+  let theFavoriteBlog = {}
+  blogs.forEach(blog => {
+    if (maxLike < blog.likes) {
+      maxLike = blog.likes
+      theFavoriteBlog = blog
+    }
+  })
+
+  if (theFavoriteBlog)
+    return {
+      title: theFavoriteBlog.title,
+      author: theFavoriteBlog.author,
+      likes: theFavoriteBlog.likes
+    }
+  else
+    return {}
+}
   
 module.exports = {
   dummy,
   totalLikes,
+  favoriteBlog
 }
