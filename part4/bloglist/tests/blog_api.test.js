44d82fb437799810205403c99aa643464c9dc071
qux-bbb <1147635419@qq.com>
2020-10-28 19:45:12 +0800

    grouped test output

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -16,63 +16,67 @@ beforeEach(async () => {
   }
 })
 
-test('there are six blogs', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-
-  expect(blogsAtStart).toHaveLength(6)
+describe('when there is initially some blogs saved', () => {
+  test('there are six blogs', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+  
+    expect(blogsAtStart).toHaveLength(6)
+  })
+  
+  test('the unique identifier property is named id', async () => {
+    const blogsAtStart = await helper.blogsInDb()
+  
+    expect(blogsAtStart[0].id).toBeDefined()
+  })
 })
 
-test('the unique identifier property is named id', async () => {
-  const blogsAtStart = await helper.blogsInDb()
-
-  expect(blogsAtStart[0].id).toBeDefined()
-})
-
-test('a valid blog can be added', async () => {
-  const newBlog = {
-    title: 'How do I pass command line arguments to a Node.js program?',
-    author: 'milkplus',
-    url: 'https://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments-to-a-node-js-program',
-    likes: 2506
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
-
-  const blogsAtEnd = await helper.blogsInDb()
-  expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
-
-  const titles = blogsAtEnd.map(blog => blog.title)
-  expect(titles).toContain(
-    'How do I pass command line arguments to a Node.js program?'
-  )
-})
-
-test('missing like attribute, default value set to 0', async () => {
-  const newBlog = {
-    title: 'How do I pass command line arguments to a Node.js program?',
-    author: 'milkplus',
-    url: 'https://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments-to-a-node-js-program',
-  }
-
-  const response = await api.post('/api/blogs').send(newBlog)
-  expect(response.body.likes).toBe(0)
-})
-
-test('blog without title and url will return 400', async () => {
-  const newBlog = {
-    author: 'milkplus'
-  }
-
-  const respose = await api.post('/api/blogs').send(newBlog)
-  expect(respose.status).toBe(400)
-  expect(respose.body).toEqual(
-    {
-      error: 'Blog validation failed: url: Path `url` is required., title: Path `title` is required.'
-    })
+describe('addition of a new note', () => {
+  test('a valid blog can be added', async () => {
+    const newBlog = {
+      title: 'How do I pass command line arguments to a Node.js program?',
+      author: 'milkplus',
+      url: 'https://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments-to-a-node-js-program',
+      likes: 2506
+    }
+  
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+  
+    const blogsAtEnd = await helper.blogsInDb()
+    expect(blogsAtEnd).toHaveLength(helper.initialBlogs.length + 1)
+  
+    const titles = blogsAtEnd.map(blog => blog.title)
+    expect(titles).toContain(
+      'How do I pass command line arguments to a Node.js program?'
+    )
+  })
+  
+  test('missing like attribute, default value set to 0', async () => {
+    const newBlog = {
+      title: 'How do I pass command line arguments to a Node.js program?',
+      author: 'milkplus',
+      url: 'https://stackoverflow.com/questions/4351521/how-do-i-pass-command-line-arguments-to-a-node-js-program',
+    }
+  
+    const response = await api.post('/api/blogs').send(newBlog)
+    expect(response.body.likes).toBe(0)
+  })
+  
+  test('blog without title and url will return 400', async () => {
+    const newBlog = {
+      author: 'milkplus'
+    }
+  
+    const respose = await api.post('/api/blogs').send(newBlog)
+    expect(respose.status).toBe(400)
+    expect(respose.body).toEqual(
+      {
+        error: 'Blog validation failed: url: Path `url` is required., title: Path `title` is required.'
+      })
+  })
 })
 
 afterAll(() => {
