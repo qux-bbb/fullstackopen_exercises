540487620bd7250d80dc49ace1761adce02435fa
qux-bbb <1147635419@qq.com>
2020-10-28 11:30:12 +0800

    4.9

--- part4/bloglist/tests/blog_api.test.js
+++ part4/bloglist/tests/blog_api.test.js
@@ -10,6 +10,12 @@ test('there is one blog', async () => {
   expect(response.body).toHaveLength(1)
 })
 
+test('the unique identifier property is named id', async () => {
+  const response = await api.get('/api/blogs')
+
+  expect(response.body[0].id).toBeDefined()
+})
+
 afterAll(() => {
   mongoose.connection.close()
 })
