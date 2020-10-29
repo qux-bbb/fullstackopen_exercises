96e3652ddcea1b2728a94aaffee5c578246191dc
qux-bbb <1147635419@qq.com>
2020-10-29 20:14:13 +0800

    4.21

--- part4/bloglist/controllers/blogs.js
+++ part4/bloglist/controllers/blogs.js
@@ -47,6 +47,19 @@ blogsRouter.put('/:id', async (request, response) => {
 })
 
 blogsRouter.delete('/:id', async (request, response) => {
+  const blog = await Blog.findById(request.params.id)
+  if (blog) {
+    const decodedToken = jwt.verify(request.token, process.env.SECRET)
+    if (!request.token || !decodedToken.id) {
+      return response.status(401).json({ error: 'token missing or invalid' })
+    }
+    if (blog.user.toString() === decodedToken.id) {
+      Blog.findByIdAndRemove(request.params.id)
+    } else {
+      return response.status(401).json({ error: 'Unauthorized' })
+    }
+  }
+
   await Blog.findByIdAndRemove(request.params.id)
   response.status(204).end()
 })
