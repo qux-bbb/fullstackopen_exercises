da7b560f9856250e03809acc7a747abde3b730ff
qux-bbb <1147635419@qq.com>
2020-11-02 13:49:46 +0800

    5.7

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,35 @@
 import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import {useState} from 'react'
+
+const Blog = ({ blog }) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showBlogDetail, setShowBlogDetail] = useState(false)
+
+  if (showBlogDetail)
+    return (
+      <div style={blogStyle}>
+        <div>
+          {blog.title} {blog.author} <button onClick={() => setShowBlogDetail(false)}>hide</button>
+        </div>
+        <div>{blog.url}</div>
+        <div>likes {blog.likes} <button>like</button></div>
+        <div>{blog.user.username}</div>
+
+      </div>
+    )
+  else
+    return (
+      <div style={blogStyle}>
+        {blog.title} {blog.author} <button onClick={() => setShowBlogDetail(true)}>view</button>
+      </div>
+    )
+}
 
 export default Blog
