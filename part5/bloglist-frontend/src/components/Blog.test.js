688f56f2676add17d0a3487b01f4469cbc7d5a53
qux-bbb <1147635419@qq.com>
2020-11-02 21:39:07 +0800

    5.14

--- part5/bloglist-frontend/src/components/Blog.test.js
+++ part5/bloglist-frontend/src/components/Blog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render } from '@testing-library/react'
+import { render, fireEvent } from '@testing-library/react'
 import Blog from './Blog'
 
 test('renders blog', () => {
@@ -16,8 +16,6 @@ test('renders blog', () => {
     <Blog blog={blog} />
   )
 
-  component.debug()
-
   expect(component.container).toHaveTextContent(
     'title1'
   )
@@ -28,6 +26,31 @@ test('renders blog', () => {
     'url1'
   )
   expect(component.container).not.toHaveTextContent(
+    '11223344'
+  )
+})
+
+test('show blog detail', () => {
+  const blog = {
+    title: 'title1',
+    author: 'author1',
+    url: 'url1',
+    likes: 11223344,
+    user: {username: 'root'}
+  }
+
+  const component = render(
+    <Blog blog={blog} />
+  )
+
+  const button = component.getByText('view')
+  fireEvent.click(button)
+
+  expect(component.container).toHaveTextContent(
+    'url1'
+  )
+
+  expect(component.container).toHaveTextContent(
     '11223344'
   )
 })
