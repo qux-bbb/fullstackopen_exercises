9612e75f02acac8241861c6bdddb65fb1b3eac1c
qux-bbb <1147635419@qq.com>
2020-11-03 17:37:27 +0800

    5.17

---
+++ part5/bloglist-frontend/cypress/integration/bloglist-frontend.spec.js
@@ -0,0 +1,14 @@
+describe('Blog app', function() {
+  beforeEach(function() {
+    cy.request('POST', 'http://localhost:3003/api/testing/reset')
+    cy.visit('http://localhost:3000')
+  })
+
+  it('Login form is shown', function() {
+    console.log(cy)
+    cy.contains('Log in to application')
+    cy.contains('username')
+    cy.contains('password')
+    cy.contains('login')
+  })
+})
