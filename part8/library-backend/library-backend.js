0a4b9aa4f3c20ec5eaed91346f9d660975b9b893
qux-bbb <1147635419@qq.com>
2020-11-10 17:15:31 +0800

    8.6 adding a book

--- part8/library-backend/library-backend.js
+++ part8/library-backend/library-backend.js
@@ -1,4 +1,5 @@
 const { ApolloServer, gql } = require('apollo-server')
+const { v1: uuid } = require('uuid')
 
 let authors = [
   {
@@ -105,6 +106,15 @@ const typeDefs = gql`
     allBooks(author: String, genre: String): [Book!]!
     allAuthors: [Author!]!
   }
+
+  type Mutation {
+    addBook(
+      title: String!
+      author: String!
+      published: Int!
+      genres: [String!]!
+    ): Book
+  }
 `
 
 const resolvers = {
@@ -126,6 +136,18 @@ const resolvers = {
   Author: {
     bookCount: (root) => {
       return books.filter(book => book.author === root.name).length
+    }
+  },
+
+  Mutation: {
+    addBook: (root, args) => {
+      const book = { ...args, id: uuid() }
+      books = books.concat(book)
+      if (!authors.find(author => author.name === args.author)){
+        const author = {name: args.author, id: uuid()}
+        authors = authors.concat(author)
+      }
+      return book
     }
   }
 }
