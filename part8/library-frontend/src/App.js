ba53e6bc87a9c510dc524a8abff1ce535360d8cd
qux-bbb <1147635419@qq.com>
2020-11-12 17:21:05 +0800

    8.20 Books by genre, step 2

--- part8/library-frontend/src/App.js
+++ part8/library-frontend/src/App.js
@@ -5,6 +5,7 @@ import Authors from './components/Authors'
 import Books from './components/Books'
 import NewBook from './components/NewBook'
 import LoginForm from './components/LoginForm'
+import Recommend from './components/Recommend'
 
 const Notify = ({ errorMessage }) => {
   if ( !errorMessage ) {
@@ -79,6 +80,7 @@ const App = () => {
         <button onClick={() => setPage('authors')}>authors</button>
         <button onClick={() => setPage('books')}>books</button>
         <button onClick={() => setPage('add')}>add book</button>
+        <button onClick={() => setPage('recommend')}>recommend</button>
         <button onClick={logout}>logout</button>
       </div>
 
@@ -92,6 +94,10 @@ const App = () => {
 
       <NewBook
         show={page === 'add'}
+      />
+
+      <Recommend
+        show={page === 'recommend'}
       />
 
     </div>
---
+++ part8/library-frontend/src/components/Recommend.js
@@ -0,0 +1,74 @@
+
+import React from 'react'
+import { gql, useQuery } from '@apollo/client'
+
+const ALL_BOOKS = gql`
+query {
+  allBooks {
+    title
+    author {
+      name
+      born
+    }
+    published
+    genres
+  }
+}
+`
+
+const ME = gql`
+query {
+  me {
+    username
+    favoriteGenre
+  }
+}
+`
+
+const Recommend = (props) => {
+  const result = useQuery(ALL_BOOKS, { pollInterval: 2000 })
+  const meResult = useQuery(ME)
+
+  if (!props.show) {
+    return null
+  }
+
+  if (result.loading || meResult.loading)  {
+    return <div>loading...</div>
+  }
+
+  const books = result.data.allBooks
+  const favoriteGenre = meResult.data.me.favoriteGenre
+  const filteredBooks = books.filter(book => book.genres.includes(favoriteGenre))
+
+  return (
+    <div>
+      <h2>recommendations</h2>
+
+      <div>books in your favorite genre <b>{favoriteGenre}</b></div>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>
+              author
+            </th>
+            <th>
+              published
+            </th>
+          </tr>
+          {filteredBooks.map(a =>
+            <tr key={a.title}>
+              <td>{a.title}</td>
+              <td>{a.author.name}</td>
+              <td>{a.published}</td>
+            </tr>
+          )} 
+        </tbody>
+      </table>
+    </div>
+  )
+}
+
+export default Recommend
