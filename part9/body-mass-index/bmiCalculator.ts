fab63e7507996b5f6c8f8c93bd1d41c054cb91c9
qux-bbb <1147635419@qq.com>
2020-11-13 19:54:30 +0800

    9.5 WebBMI

--- part9/body-mass-index/bmiCalculator.ts
+++ part9/body-mass-index/bmiCalculator.ts
@@ -1,24 +1,4 @@
-interface MultiplyValues {
-  height: number;
-  mass: number;
-}
-
-const parseArguments = (args: Array<string>): MultiplyValues => {
-  if (args.length < 4) throw new Error('Not enough arguments');
-  if (args.length > 4) throw new Error('Too many arguments');
-
-  if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
-    return {
-      height: Number(args[2]),
-      mass: Number(args[3])
-    }
-  } else {
-    throw new Error('Provided values were not numbers!');
-  }
-}
-
-const calculateBmi = (height: number, mass: number): string => {
-  if (height === 0) throw new Error('height can not be 0')
+export const calculateBmi = (height: number, mass: number): string => {
   const hightByM: number = height / 100;
   const bmi: number = mass / (hightByM * hightByM);
   switch (true) {
@@ -39,12 +19,4 @@ const calculateBmi = (height: number, mass: number): string => {
     default:
       return 'Very severely underweight';
   }
-  
 };
-
-try {
-  const { height, mass } = parseArguments(process.argv);
-  console.log(calculateBmi(height, mass));
-} catch (e) {
-  console.log('Error, something bad happened, message: ', e.message);
-}
--- part9/body-mass-index/index.ts
+++ part9/body-mass-index/index.ts
@@ -1,11 +1,36 @@
 import express from 'express';
+import { calculateBmi } from "./bmiCalculator";
+
 const app = express();
+
+const checkQuery = (query: any): boolean => {
+  if (!query.height || !query.weight) 
+    return false
+  if (isNaN(Number(query.height)) || isNaN(Number(query.weight)))
+    return false
+  if (query.height === 0)
+    return false
+  return true
+}
 
 app.get('/Hello', (_req, res) => {
   res.send('Hello Full Stack!');
 });
 
-const PORT = 3003;
+app.get('/bmi', (req, res) => {
+  if (checkQuery(req.query)) {
+    const height: number = Number(req.query.height);
+    const weight: number = Number(req.query.weight);
+    const bmi: string = calculateBmi(height, weight);
+    res.json({ weight, height, bmi })
+  } else {
+    res.json({ error: "malformatted parameters" });
+  }
+});
+
+
+
+const PORT = 3002;
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
