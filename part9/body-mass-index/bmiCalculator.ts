0dcc3d393ee9362cf89e6e3e746af6c53ce6db4e
qux-bbb <1147635419@qq.com>
2020-11-13 15:31:49 +0800

    9.1 body mass index

---
+++ part9/body-mass-index/bmiCalculator.ts
@@ -0,0 +1,26 @@
+const calculateBmi= (height: number, mass: number) => {
+  if (height === 0) throw new Error('height can not be 0')
+  const hightByM = height / 100;
+  const bmi = mass / (hightByM * hightByM);
+  switch (true) {
+    case bmi>40:
+      return 'Obese Class III (Very severely obese)';
+    case bmi>35:
+      return 'Obese Class II (Severely obese)';
+    case bmi>30:
+      return 'Obese Class I (Moderately obese)';
+    case bmi>25:
+      return 'Overweight';
+    case bmi>18.5:
+      return 'Normal (healthy weight)';
+    case bmi>16:
+      return 'Underweight';
+    case bmi>15:
+      return 'Severely underweight';
+    default:
+      return 'Very severely underweight';
+  }
+  
+};
+
+console.log(calculateBmi(180, 74));
