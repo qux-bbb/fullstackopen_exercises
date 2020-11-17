401dffd85a59a82e3b2a6bce664a6ad1e25f8b7f
qux-bbb <1147635419@qq.com>
2020-11-18 00:06:29 +0800

    9.10 patientor backend, step3

---
+++ part9/patientor-backend/src/services/diagnoseService.ts
@@ -0,0 +1,10 @@
+import diagnoseData from '../../data/diagnoses.json'
+import { Diagnose } from "../types";
+
+const getEntries = (): Array<Diagnose> => {
+  return diagnoseData;
+};
+
+export default {
+  getEntries
+};
